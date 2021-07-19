35fd2a93c98d212287cbedc68d80e6a6873f58bf
Alejandro Medina <5almero5@gmail.com>
2021-07-19 12:17:31 -0500

    add switch condition

--- src/main.go
+++ src/main.go
@@ -102,4 +102,47 @@ func main() {
 	if condition1 == 0 || condition2 == 2 {
 		fmt.Println("Verdad")
 	}
+
+	// SWITCH
+	// Estructura condicional - Switch
+	// Si tienes una variable y conoses los valores
+	// de esta variable este es tu mejor uso
+	const dia int8 = 4
+
+	switch dia {
+	case 1:
+		fmt.Println("Lunes")
+	case 2:
+		fmt.Println("Martes")
+	case 3:
+		fmt.Println("Miercoles")
+	case 4:
+		fmt.Println("Jueves")
+	case 5:
+		fmt.Println("Viernes")
+	case 6:
+		fmt.Println("Sabado")
+	case 7:
+		fmt.Println("Domingo")
+	default:
+		fmt.Println("Ese no es un día valido de la semana!")
+	}
+	// otra forma
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("Es Par")
+	default:
+		fmt.Println("Es impar")
+	}
+	// swich sin condicion
+	// Si vas a revisar multiples aspectos o codiciones detenrminadas
+	option := 200
+	switch {
+	case option > 100:
+		fmt.Println("Es mayor que 100")
+	case option < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No Condition")
+	}
 }
