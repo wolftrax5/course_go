374361ac798ba9a94277138acf20eb09aa7b022c
Alejandro Medina <5almero5@gmail.com>
2021-07-18 20:07:51 -0500

    Operaciones aritmeticas

--- src/main.go
+++ src/main.go
@@ -26,4 +26,45 @@ func main() {
 	const baseCuadrado = 10 // go asigna el valor y el tipo en vase al valor
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	// Aritmetica
+	x := 10
+	y := 50
+	// Suma
+	result := x + y // usamos los : por que a sido declarada anteriormente
+	fmt.Println(x, "+", y, "=", result)
+	//resta
+	result = x - y // no usamos los : ya fue declarada
+	fmt.Println(x, "-", y, "=", result)
+	// multiplicacion
+	result = x * y
+	fmt.Println(x, "*", y, "=", result)
+	// Divicion
+	result = y / x
+	fmt.Println(y, "/", x, "=", result)
+	// Modulo (reciduo)
+	result = y % x
+	fmt.Println(y, "/", x, "Modulo(residuo):", result)
+	// incremental
+	x++
+	fmt.Println("incremental", x)
+	// decremental
+	x--
+	fmt.Println("decremental", x)
+
+	height := 40
+	var rArea = base * height
+	fmt.Println("Area del rectangulo: ", rArea)
+
+	higher := 90
+	less := 45
+	size := 55
+	var tArea = ((higher + less) * size / 2)
+	fmt.Println("Area del trapecio: ", tArea)
+
+	radio := 45.5
+	const pi = 3.14
+	var cArea = (radio * radio) * pi
+	fmt.Println("Area del circulo: ", cArea)
+
 }
