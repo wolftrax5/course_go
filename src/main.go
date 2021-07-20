5e4ad3187a9430d630d6e1469991a6f748f99f16
Alejandro Medina <5almero5@gmail.com>
2021-07-20 17:33:35 -0500

    manejo de Slices

--- src/main.go
+++ src/main.go
@@ -3,32 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	// Defer
-	/*
-		** defer: ** ejecuta la parte de código especificada al final
-		de la ejecución de la función o método.
-		Como buena práctica usar solo una sentencia “defer”
-		dentro de un bloque.
-		Ejemplo: abres conexion a una BD un defear que cierre la conexion
-		Cuando abres un archivo como defer para cerrarlo.
-	*/
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
-	// Continue y Break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		// continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-			// Se utiliza segun cuando una condicion dada
-			// se interesa que se continua como un error controlado
-		}
-		// Break
-		if i == 8 {
-			fmt.Println("Break")
-			// rompera el ciclo
-			break
-		}
-	}
+	// Array INMUTABLE
+	fmt.Println("ARRAY")
+	var array [4]int8
+	// se auto llena con los zerovalues
+	fmt.Println(array)
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+	fmt.Println("SLICE")
+	// Slice MUTABLE
+	// muy similar a Array pero
+	// No se indica la cantidad de valores que tendra
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos de Slice
+	fmt.Println(slice[0])  // imprime el valor del indice 0
+	fmt.Println(slice[:3]) // imprime el valor hasta el indice 3
+	// el numero despuesde : es exclusivo
+	fmt.Println(slice[2:4]) // imprime los valores entre el indice 2 y 4
+	// el primer numero es inclusivo y el segundo exclusivo
+	fmt.Println(slice[4:]) // imprime del indice 4 en delante
+
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+	// appen nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
