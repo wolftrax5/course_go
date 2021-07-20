7c5fa97812313517eae2577398ad7cf18f647807
Alejandro Medina <5almero5@gmail.com>
2021-07-20 17:47:10 -0500

    Add slice iteration

--- src/main.go
+++ src/main.go
@@ -1,36 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(text string) {
+	var textReverse string
+
+	text = strings.ToLower(text)
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+	if text == textReverse {
+		fmt.Println(text, " Es palindromo")
+	} else {
+		fmt.Println(text, " No es palindromo")
+	}
+}
 
 func main() {
-	// Array INMUTABLE
-	fmt.Println("ARRAY")
-	var array [4]int8
-	// se auto llena con los zerovalues
-	fmt.Println(array)
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
-	fmt.Println("SLICE")
-	// Slice MUTABLE
-	// muy similar a Array pero
-	// No se indica la cantidad de valores que tendra
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	var slice = []string{"hola", "que", "haces"}
+	// de esta forma iteramos obteniendo el indice y el valor
+	// ya que range retorna estos 2 valores
+	for i, varlor := range slice {
+		fmt.Println("index:", i, "value:", varlor)
+	}
+	// con _ (piso) escapamos el uso de indice el cual no usamos
+	for _, varlor := range slice {
+		fmt.Println("value:", varlor)
+	}
+	// con _ (piso) escapamos el uso de indice el cual no usamos
+	for i := range slice {
+		fmt.Println("index:", i)
+	}
 
-	// Metodos de Slice
-	fmt.Println(slice[0])  // imprime el valor del indice 0
-	fmt.Println(slice[:3]) // imprime el valor hasta el indice 3
-	// el numero despuesde : es exclusivo
-	fmt.Println(slice[2:4]) // imprime los valores entre el indice 2 y 4
-	// el primer numero es inclusivo y el segundo exclusivo
-	fmt.Println(slice[4:]) // imprime del indice 4 en delante
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-	// appen nueva list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	isPalindrome("ama")
 }
