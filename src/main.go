0c0f51a886d2533d82c01789cf3b712e8955be01
Alejandro Medina <5almero5@gmail.com>
2021-07-21 14:28:04 -0500

    Use of Struct

--- src/main.go
+++ src/main.go
@@ -2,32 +2,23 @@ package main
 
 import "fmt"
 
+// kyword para hacer estrucutras de datos
+type car struct {
+	brand string
+	year  int16
+}
+
 func main() {
-	// con make podemos crear diccionarios
-	// u otros tipos de variables
-	// map[typo de la llave]tipo de valor
-	m := make(map[string]int)
-
-	m["Jose"] = 13
-	m["Alex"] = 20
-	// en las impreciones por consola
-	// no se separan por , sino por espacios
-	fmt.Println(m) // map[Alex:20 Jose:13]
-
-	// Recorrido de un map
-	for i, v := range m {
-		fmt.Println(i, v)
-	}
-	// Encontrar un valor
-	value := m["Jose"]
-	fmt.Println(value)
-	// a un valor no encontrado usara el Zero Value
-	value = m["Josep"]
-	fmt.Println(value)
-	// podemos declarar otra variable
-	// nos retorna un booleando
-	// indicando que la llave/valor exite en el Map
-	value2, ok := m["Josep"]
-	fmt.Println(value2, ok)
-
+	var myCar = car{brand: "Ford", year: 2019}
+	otherCar := car{brand: "Nissan", year: 2021}
+	fmt.Println(myCar)
+	fmt.Printf("myCar type: %T \n", myCar)
+	fmt.Println(otherCar)
+	fmt.Printf("otherCar type: %T \n", otherCar)
+	// Tambien podemos instanciar de vorma vacia
+	// tomara los zero values en donde no asignemos algo
+	var nextCar car
+	nextCar.brand = "Ferrari"
+	fmt.Println(nextCar)
+	fmt.Printf("nextCar type: %T \n", nextCar)
 }
