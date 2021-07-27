9348d12fb203c6c1eef55e34d88ddcfbc1cf386f
Alejandro Medina <5almero5@gmail.com>
2021-07-27 15:32:36 -0500

    Uso de interfaces y referencias

--- src/main.go
+++ src/main.go
@@ -1,32 +1,53 @@
 package main
 
-import (
-	pc "course_go/src/mypc"
-	"fmt"
-)
+import "fmt"
+
+/*
+Si los structs que tenemos en el código tienen métodos
+que hacen algo en común
+(Cálculos, obtener data, etc),
+es posible ejecutar éstos métodos usando una interfaz,
+de esta forma evitamos hacer código por cada struct.
+*/
+type figuras2D interface {
+	area() float64 // tine que ser exactamente le nombre de la funcion
+}
+type cuadrado struct {
+	base float64
+}
+type rectangulo struct {
+	base   float64
+	altura float64
+}
+
+func (c cuadrado) area() float64 {
+	// ser repite el nombre pero no el tipo de dato acetado
+	// o la logica de ejecucion
+	return c.base * c.base
+}
+func (r rectangulo) area() float64 {
+	return r.base * r.altura
+}
+
+func calcular(f figuras2D) {
+	fmt.Println("Area", f.area())
+}
 
 func main() {
-	a := 50
-	b := &a
-	// el & es para accder a le direccion
-	// de memoria
-	fmt.Println("b", b)
-	// y el * es para acceder al valor
-	// de la direccino de memoria
-	fmt.Println("*b", *b)
-	*b = 100
-	// cualquier modificacion a la direccion de memoria
-	// se modificara en todas sus referencias
-	fmt.Println("a", a)
+	myCuadrado := cuadrado{base: 2}
+	myRectanculo := rectangulo{base: 2, altura: 4}
+	/*
+		el mejor punto de usar una interface
+		es cuando los structs comparten una misma funcion
+		para su reutilizacion
+	*/
+	calcular(myCuadrado)
+	calcular(myRectanculo)
 
-	// ejemplo en un modulo
-	myPc := pc.New(12, 200, "HP")
-	myPc.SetRam(16)
-	myPc.FormatPrint()
-	fmt.Println("Se duplica la ram")
-	myPc.DuplicateRAM()
-	myPc.FormatPrint()
-
-	// usando el output Personalizado
-	fmt.Println(myPc)
+	// listas de interfaces
+	// por defecto go no hacepta slice de diferente tipos
+	// pero con interfaces podemos hacerlos
+	myInterface := []interface{}{"Hola", 121, 4.90}
+	// los ... destricitran cada uno delos elementos
+	fmt.Println(myInterface...)
 }
