7585020b34f04ac9989b12bff4f0656fa5e87c7b
Alejandro Medina <5almero5@gmail.com>
2021-07-30 19:32:47 -0500

    Canales entrada y salida

--- src/main.go
+++ src/main.go
@@ -1,51 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println(text)
+// indicamos que este canal eso solo de entrada
+// lo contrario seria c <-chan
+func say(text string, c chan<- string) {
+	// c <- esto indica que vamos a ingresar un dato
+	// en este canal
+	c <- text
+
 }
 
-// la funcion main esta corriendo dentro de
-// una go rutine, una ves ejecutada muere
 func main() {
-	/*
-		Un WaitGroup espera a que una colección de goroutines
-		termine su ejecución.
-		Para esto se una la WaitGroup.Add()
-		( wg.add(1) en el ejemplo de la clase).
-		El número entero indica el número de goroutines
-		que debe esperar para finalizar
-		la ejecución de la goroutine principal.
-
-		Cada vez que una goroutine termina
-		su ejecución, llama el método Done().
-		Esto hace que el contador del WaitGroup se
-		reduzca.
-		Cuando el contador llegue a zero la rutina principal
-		continuará su ejecución.
-
-		La función wait() bloquea la rutina principal
-		hasta que todas las demás rutinas del grupo hayan terminado.
-	*/
-	var wg sync.WaitGroup
-
-	fmt.Println("Hello")
-	wg.Add(1)
-	// para manejar las go runites es necesario
-	// usar la keyword go antes de la funcion
-	go say("World", &wg)
-
-	wg.Wait()
-
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1)
+	c := make(chan string, 1)
+	fmt.Println("HEllo")
+	go say("Bye", c)
+	// <-c esto indica que vamos a obtener la salida
+	// un dato en este canal
+	fmt.Println(<-c)
 }
